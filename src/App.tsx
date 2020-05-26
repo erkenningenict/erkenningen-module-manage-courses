87f2a74fe59890ab4596ab6eb0c17353c32ab8f4
Gerwin Koudijs <>
2020-05-26 08:39:34 +0200

    feat: implement route switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return <div className="App">Module manage courses to be developed</div>;
-}
+const App: React.FC<{}> = (props) => {
+  return (
+    <Switch>
+      <Route path="/edit">Edit courses</Route>
+      <Route path="/new">Create course</Route>
+      <Route path="/">
+        Route not found, please set a route in the url hash (e.g. /edit or /new)
+      </Route>
+    </Switch>
+  );
+};
 
 export default App;

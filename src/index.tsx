24f0c7d299cff0af3db37bfbabc5b020548052d7
Gerwin Koudijs <>
2020-05-26 08:35:35 +0200

    chore: add router package types

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { HashRouter } from "react-router-dom";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("erkenningen-module-manage-courses")
 );

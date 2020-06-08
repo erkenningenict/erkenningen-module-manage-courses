008e47a5dbef8b7febaf9e6a30d6fa2d762d391c
Gerwin Koudijs <>
2020-06-08 16:09:07 +0200

    feat: implementing form controls

--- src/courses/edit/CourseEdit.tsx
+++ src/courses/edit/CourseEdit.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import {
   Panel,
@@ -6,7 +6,10 @@ import {
   FormSelect,
   FormText,
   FormCalendar,
-} from "@erkenningen/ui";
+  FormCheckbox,
+  FormItem,
+  Button,
+} from '@erkenningen/ui';
 
 const CourseEdit: React.FC<{}> = (props) => {
   return (
@@ -14,57 +17,52 @@ const CourseEdit: React.FC<{}> = (props) => {
       <Panel title="Nieuwe bijeenkomst maken">
         <FormSelect
           labelClassNames="col-sm-12"
-          placeholder={"Selecteer een kennisaanbieder"}
-          id={"organizer"}
-          label={
-            "Kies de kennisaanbieder waarvoor u een nieuwe cursus wilt maken"
-          }
+          placeholder={'Selecteer een kennisaanbieder'}
+          id={'organizer'}
+          label={'Kies de kennisaanbieder waarvoor u een nieuwe cursus wilt maken'}
           options={[]}
         />
         <FormSelect
           labelClassNames="col-sm-12"
-          placeholder={"Selecteer een kennisaanbod"}
-          helpText={"Geldig van 25-05-2020 tot 23-09-2022 TODO!"}
-          id={"specialty"}
-          label={
-            "Kies het kennisaanbod waarop u de nieuwe bijeenkomst wilt baseren:"
-          }
+          placeholder={'Selecteer een kennisaanbod'}
+          helpText={'Geldig van 25-05-2020 tot 23-09-2022 TODO!'}
+          id={'specialty'}
+          label={'Kies het kennisaanbod waarop u de nieuwe bijeenkomst wilt baseren:'}
           options={[]}
         />
       </Panel>
       <Panel title="Bijeenkomst">
         <Form onSubmit={() => {}} className="hello">
-          <FormText id={"titel"} label={"Titel"} />
+          <FormText id={'titel'} label={'Titel'} />
+          <FormText id={'promotieTekst'} label={'Promotietekst'} isTextArea={true} />
           <FormText
-            id={"promotieTekst"}
-            label={"Promotietekst"}
-            isTextArea={true}
+            id={'prijsPerDeelnemer'}
+            label={'Prijs per deelnemer'}
+            formControlClassName="col-sm-2"
+            placeholder={'0,00'}
           />
-          <FormText id={"prijsPerDeelnemer"} label={"Prijs per deelnemer"} />
           <FormText
-            id={"maxAantalDeelnemers"}
-            label={"Max. aantal deelnemers"}
+            id={'maxAantalDeelnemers'}
+            label={'Max. aantal deelnemers'}
+            formControlClassName="col-sm-2"
+            placeholder={'1'}
           />
-          <FormText id={"besloten"} label={"Besloten"} />
-          <FormText
-            id={"opmerkingen"}
-            label={"Opmerkingen"}
-            isTextArea={true}
-          />
-          <FormCalendar id={"datum"} label={"Datum"} />
-          <FormCalendar id={"beginTijd"} label={"Begintijd"} />
-          <FormCalendar id={"eindTijd"} label={"Eindtijd"} />
+          <FormCheckbox id={'besloten'} label={'Besloten'} value={true} />
+          <FormText id={'opmerkingen'} label={'Opmerkingen'} isTextArea={true} />
+          <FormCalendar id={'datum'} label={'Datum'} formControlClassName="col-sm-3" />
+          <FormCalendar id={'beginTijd'} label={'Begintijd'} formControlClassName="col-sm-3" />
+          <FormCalendar id={'eindTijd'} label={'Eindtijd'} formControlClassName="col-sm-3" />
           <FormSelect
-            id={"locatie"}
-            label={"Locatie"}
+            id={'locatie'}
+            label={'Locatie'}
             options={[]}
-            placeholder={"Selecteer een locatie"}
+            placeholder={'Selecteer een locatie'}
+            formControlClassName="col-sm-5"
           />
-          <FormText
-            id={"docent"}
-            label={"Docent(en)"}
-            placeholder={"Voer optioneel docenten in"}
-          />
+          <FormText id={'docent'} label={'Docent(en)'} placeholder={'Voer optioneel docenten in'} />
+          <FormItem label={' '}>
+            <Button label={'Opslaan'} />
+          </FormItem>
         </Form>
       </Panel>
     </>

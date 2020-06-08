cd39df531e423a75b08e2b6c0a5f270e5f37d222
Gerwin Koudijs <>
2020-06-08 09:38:36 +0200

    style: text change

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { Switch, Route } from "react-router-dom";
 const App: React.FC<{}> = (props) => {
   return (
     <Switch>
-      <Route path="/edit">Edit courses</Route>
+      <Route path="/edit">Edit course</Route>
       <Route path="/new">Create course</Route>
+      <Route path="/list">List courses</Route>
       <Route path="/">
         Route not found, please set a route in the url hash (e.g. /edit or /new)
       </Route>
